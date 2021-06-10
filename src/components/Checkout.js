e3f3978067a25c2e671f9f1aa35903b423713b5d
Imran Mohamed <ibm786@icloud.com>
2021-06-10 18:33:12 +0100

    added react router dom and added checkout component

---
+++ src/components/Checkout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Checkout.css';
+
+const Checkout = () => {
+  return (
+    <div className='checkout'>
+      <div className='checkout__left'></div>
+      <div className='checkout__right'></div>
+    </div>
+  );
+};
+
+export default Checkout;

1b15931d3db849d96a9cecada71d0eccb2cdbd61
Imran Mohamed <ibm786@icloud.com>
2021-06-10 15:13:22 +0100

    create react app and cleanup and firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDuC7b0qJNB3iCjFy9e1TDtuFMprOEwJ0Q',
+  authDomain: 'clone-faa4c.firebaseapp.com',
+  projectId: 'clone-faa4c',
+  storageBucket: 'clone-faa4c.appspot.com',
+  messagingSenderId: '508724837698',
+  appId: '1:508724837698:web:ca85f9ca7a28d115601cdb',
+};
